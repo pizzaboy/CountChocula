7c6accd7ae25e458035c40cbf4ab0d2b9f2837d2
Peter Yates <peter.yates@deltafs.co.uk>
2020-05-07 15:42:44 +0100

    Add more chocolatey modules and change order

--- InstallCountChocula.ps1
+++ InstallCountChocula.ps1
@@ -3,17 +3,21 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Assign Packages to Install
-$Packages = 'googlechrome',`
+$Packages = 'sql-server-2019',`
+            'git',`
+            'googlechrome',`
+            'dotnetcore-sdk',`
+            'dotnetcore-runtime',`
             'adoptopenjdk11jre',`
             'notepadplusplus',`
-            'sql-server-2019',`
             'netfx-4.7.2-devpack'
 
 #Install Packages
 ForEach ($PackageName in $Packages)
 {choco install $PackageName -y}
 
-install-module -name AWSPowershell -Force
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+install-module -name AWSPowershell -Force -Scope AllUsers
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
 
